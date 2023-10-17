136ecf4bed77cb39f128c4845dc123556b31edb0
alachhman <33943685+alachhman@users.noreply.github.com>
2023-10-17 00:10:12 -0400

    fixes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,48 +8,22 @@ function App() {
                 <div className={"Image-container"}>
                     <img src={EE} className="App-logo" alt="logo"/>
                     <div className={"Text"}>
-                        <p>To get started, paste this into your Node project:</p>
+                        <p>To get started quickly, copy and paste this into</p>
+                        <p>your Node project, then run 'npm install' !</p>
                         <p style={{textAlign: "center"}}>-</p>
                         <div className={"Code-block"}>
-                            <p>
-                                <span style={{color: "indianred"}}>import</span>
-                                <span style={{color: "green"}}>fetch</span>
-                                <span style={{color: "indianred"}}>from</span>
-                                <span style={{color: "lightseagreen"}}>"node-fetch"</span>;
+                            <p><span style={{color: "indianred"}}>import </span><span style={{color: "green"}}>fetch </span><span style={{color: "indianred"}}>from </span><span style={{color: "lightseagreen"}}>"node-fetch"</span>;
                             </p>
-                            <p>
-                                <span style={{color: "green"}}>fetch</span>
-                                (
-                                <span style={{color: "lightseagreen"}}>"https://www.FE-API.com/fe8/units"</span>
-                                )
+                            <p><span style={{color: "green"}}>fetch</span>(<span style={{color: "lightseagreen"}}>"https://www.FE-API.com/fe8/units"</span>)
                             </p>
-                            <p className={"indent"}>
-                                .
-                                <span style={{color: "blue"}}>then</span>
-                                ((
-                                <span style={{color: "green"}}>response</span>
-                                ) =>
-                                <span style={{color: "green"}}>response</span>
-                                <span style={{color: "blue"}}>.json</span>
-                                ())
+                            <p className={"indent"}>.<span style={{color: "blue"}}>then</span>((<span style={{color: "green"}}>response</span>) => <span style={{color: "green"}}>response</span><span style={{color: "blue"}}>.json</span>())
                             </p>
-                            <p className={"indent"}>
-                                .
-                                <span style={{color: "blue"}}>then</span>
-                                ((
-                                <span style={{color: "green"}}>data</span>
-                                ) =>
-                                <span style={{color: "indianred"}}>console</span>
-                                .
-                                <span style={{color: "blue"}}>log</span>
-                                (
-                                <span style={{color: "green"}}>data</span>
-                                ));
+                            <p className={"indent"}>.<span style={{color: "blue"}}>then</span>((<span style={{color: "green"}}>data</span>) => <span style={{color: "indianred"}}>console</span>.<span style={{color: "blue"}}>log</span>(<span style={{color: "green"}}>data</span>));
                             </p>
                         </div>
                         <p style={{textAlign: "center"}}>-</p>
-                        <p>Or feel free to explore the other endpoints.</p>
-                        <p>Under construction - More coming soon!</p>
+                        <p>Feel free to explore the other endpoints.</p>
+                        <p>This is under construction - More coming soon !</p>
                     </div>
                 </div>
             </header>
--- frontend/src/test.js
+++
@@ -1,5 +0,0 @@
-import fetch from "node-fetch";
-
-fetch("https://www.FE-API.com/fe8/units")
-    .then((response) => response.json())
-    .then((data) => console.log(data));
